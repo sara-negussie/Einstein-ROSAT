c8e2a2755e810edc8626ab33f7f56444a9a7aab6
Sara Negussie <snegussie@wesleyan.edu>
2022-06-21 17:34:50 -0400

    now works with offsets

---
+++ test.c
@@ -0,0 +1,40 @@
+    //return *theta, *DEC_E, *DEC_R, *RA_E, *RA_R, *size;
+
+
+    //coor_data(DEC_Einstein, DEC_ROSAT, RA_Einstein, RA_ROSAT, &theta, &dec_E, &dec_R, &RA_E, &RA_R, &size_file)
+
+    //int size_file;
+    //float theta, dec_E, dec_R, RA_E, RA_R;
+    //theta, dec_E, dec_R, RA_E, RA_R, size_file = coor_data(DEC_Einstein, DEC_ROSAT, RA_Einstein, RA_ROSAT)
+/*
+    printf("End...");
+    size(theta);
+    ang_sep = zeros(size_file);
+    for i in range(size(theta)_
+        ang_sep[i] = round(theta[i].value, 1)
+
+    dec_E_new = zeros(size_file)
+    for i in range(size(theta)):
+        dec_E_new[i] = round(dec_E[i].value, 1)
+
+    dec_R_new = zeros(size_file)
+    for i in range(size(theta)):
+        dec_R_new[i] = round(dec_R[i].value, 1)
+
+    ra_E_new = zeros(size_file)
+    for i in range(size(theta)):
+        ra_E_new[i] = round(RA_E[i].value, 1)
+
+    ra_R_new = zeros(size_file)
+    for i in range(size(theta)):
+        ra_R_new[i] = round(RA_R[i].value, 1)
+
+    print("Writing file")
+
+    with open(r'angular_sep.txt', 'w') as fp:
+            for i in range(size(theta)):
+                fp.write("Angular Sep = %f\t DEC Einstein = %f\t DEC ROSART = %f\t RA Einstein = %f\t RA ROSART = %f\n" 
+                        % ((ang_sep[i]), (dec_E_new[i]), (dec_R_new[i]), (ra_E_new[i]), (ra_R_new[i])))
+                
+    print("finished")
+*/
